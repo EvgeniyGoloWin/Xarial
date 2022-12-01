f50e76da3b5176148a5eccb43cf8e0db2db91a93
Evgeniy <golowin056@gmail.com>
2022-12-01 23:37:29 +0200

    bag fix

--- app.js
+++ app.js
@@ -8,38 +8,46 @@ let form = document.getElementById('form'),
     inputName = document.querySelector('.js-input-name'),
     inputCompany = document.querySelector('.js-input-company'),
     inputCountry = document.querySelector('.js-input-country');
+    inputRadioBtn = document.querySelector('.radioBtn')
+    inputDate = document.querySelector('.date')
     btnNext = document.getElementById('next')
     btnBack = document.getElementsByName('back')
-    // btnBack1 = document.getElementById('back1')
-    // btnBack2 = document.getElementById('back2')
     answer = document.getElementById('answer')
     document.querySelector('.js-input-checkbox');
 
+console.log(inputRadioBtn)
+console.log(inputDate)
+console.log(document.querySelector('.js-input-checkbox'))
 
-//     answer.onchange = (e) => {
-//     selectValue = e.target.value
+// function validateEmail(email) {
+//     let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
 // }
-
-function validateEmail(email) {
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    console.log(re)
-    return re.test(String(email).toLowerCase());
-}
-
-function validateName(name) {
-    let re = /[a-z]/;
-    return re.test(String(name).toLowerCase());
-}
-
-function validateCompany(company) {
-    let re = /[a-z]/;
-    return re.test(String(company).toLowerCase());
-}
-
-function validateCountry(country) {
-    let re = /[a-z]/;
-    return re.test(String(country).toLowerCase());
-}
+//
+// function validateName(name) {
+//     let re = /[a-z]/;
+//     return re.test(String(name).toLowerCase());
+// }
+//
+// function validateCompany(company) {
+//     let re = /[a-z]/;
+//     return re.test(String(company).toLowerCase());
+// }
+//
+// function validateCountry(country) {
+//     let re = /[a-z]/;
+//     return re.test(String(country).toLowerCase());
+// }
+//
+// function validateRadioBtn(select) {
+//     let re = /[a-z]/;
+//     return re.test(String(select).toLowerCase());
+// }
+//
+// function validateDate(date) {
+//     let re = /[a-z]/;
+//     return re.test(String(date).toLowerCase());
+// }
 
 let radioValue
 
@@ -61,10 +69,7 @@ btnNext.onclick = () => {
             break;
     }
 }
-// btnBack.onclick = () => {
-//     form2.classList.add('hidden')
-//     form1.classList.remove('hidden')
-// }
+console.log(btnBack)
 for (let i = 0, length = btnBack.length; i < length; i++) {
     btnBack[i].onclick = (e) => {
         switch (e.target.value) {
@@ -80,14 +85,6 @@ for (let i = 0, length = btnBack.length; i < length; i++) {
         }
     }
 }
-// btnBack1.onclick = () => {
-//     form3.classList.add('hidden')
-//     form1.classList.remove('hidden')
-// }
-// btnBack2.onclick = () => {
-//     form4.classList.add('hidden')
-//     form1.classList.remove('hidden')
-// }
 
 form.onsubmit = function (e) {
     e.preventDefault()
@@ -95,51 +92,74 @@ form.onsubmit = function (e) {
         nameVal = inputName.value,
         companyVal = inputCompany.value,
         countryVal = inputCountry.value,
-        emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+        radioVal = inputRadioBtn.value,
+        dateVal = inputDate.value,
+        emptyInputs = Array.from(formInputs).filter(input => input.value === '')
+        const formData = new FormData();
+
 
     formInputs.forEach(function (input) {
-        if (input.value === '') {
-            input.classList.add('error');
-            console.log('input not field')
-        } else {
-            input.classList.remove('error');
-        }
+        formData.append(`${input.name}`, `${input.value}`);
+        // if (input.value === '') {
+        //     input.classList.add('error');
+        //     console.log('input not field')
+        // } else {
+        //     input.classList.remove('error');
+        // }
+        console.log(input.value)
+        console.log(input.name)
     });
+    console.log(formData)
+    // if (emptyInputs.length !== 0) {
+    //     console.log('inputs not filled');
+    //     return false;
+    // }
+    //
+    // if(!validateEmail(emailVal)) {
+    //     console.log('email not valid');
+    //     inputEmail.classList.add('error');
+    //     return false;
+    // } else {
+    //     inputEmail.classList.remove('error');
+    // }
+    //
+    // if(!validateName(nameVal)) {
+    //     console.log('name not valid')
+    //     inputName.classList.add('error');
+    //     return false;
+    // } else {
+    //     inputName.classList.remove('error');
+    // }
+    //
+    // if(!validateCompany(companyVal)) {
+    //     console.log('company not valid')
+    //     inputCompany.classList.add('error');
+    //     return false;
+    // } else {
+    //     inputCompany.classList.remove('error');
+    // }
+    //
+    // if(!validateCountry(countryVal)) {
+    //     console.log('country not valid')
+    //     inputCountry.classList.add('error');
+    //     return false;
+    // } else {
+    //     inputCountry.classList.remove('error');
+    // }
 
-    if (emptyInputs.length !== 0) {
-        console.log('inputs not filled');
-        return false;
-    }
-
-    if(!validateEmail(emailVal)) {
-        console.log('email not valid');
-        inputEmail.classList.add('error');
-        return false;
-    } else {
-        inputEmail.classList.remove('error');
-    }
-
-    if(!validateName(nameVal)) {
-        console.log('name not valid')
-        inputName.classList.add('error');
-        return false;
-    } else {
-        inputName.classList.remove('error');
-    }
-
-    if(!validateCompany(companyVal)) {
-        console.log('company not valid')
-        inputCompany.classList.add('error');
-        return false;
-    } else {
-        inputCompany.classList.remove('error');
-    }
-
-    if(!validateCountry(countryVal)) {
-        console.log('country not valid')
-        inputCountry.classList.add('error');
-        return false;
-    } else {
-        inputCountry.classList.remove('error');
-    }
+    // if(!validateRadioBtn(radioVal)) {
+    //     console.log('please make a choice')
+    //     inputRadioBtn.classList.add('error');
+    //     return false;
+    // } else {
+    //     inputRadioBtn.classList.remove('error');
+    // }
+    //
+    // if(!validateDate(dateVal)) {
+    //     console.log('please select date')
+    //     inputDate.classList.add('error');
+    //     return false;
+    // } else {
+    //     inputDate.classList.remove('error');
+    // }
 }
