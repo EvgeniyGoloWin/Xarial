01304504693366ae4eed02726dce2b85e5a40b6f
marchinmrochik <kiselroman280@gmail.com>
2022-12-02 17:55:30 +0200

    added validate and class for error validate

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const radioBtn = document.getElementsByName('1');
 let form = document.getElementById('form'),
-    form1 = document.getElementById('main-block')
+    form1 = document.getElementById('main-block'),
     form2 = document.getElementById('main-block_1'),
     form3 = document.getElementById('main-block_2'),
     form4 = document.getElementById('main-block_3'),
@@ -7,69 +8,25 @@ let form = document.getElementById('form'),
     inputEmail = document.querySelector('.js-input-email'),
     inputName = document.querySelector('.js-input-name'),
     inputCompany = document.querySelector('.js-input-company'),
-    inputCountry = document.querySelector('.js-input-country');
-    inputRadioBtn = document.querySelector('.radioBtn')
-    inputDate = document.querySelector('.date')
-    btnNext = document.getElementById('next')
-    btnBack = document.getElementsByName('back')
-    answer = document.getElementById('answer')
-    document.querySelector('.js-input-checkbox');
+    inputCountry = document.querySelector('.js-input-country'),
+    inputRadioBtn = document.querySelector('.radioBtn'),
+    inputDate = document.querySelector('.date'),
+    btnNext = document.getElementById('next'),
+    btnBack = document.getElementsByName('back'),
+    answer = document.getElementById('answer');
+let radioValue;
+let state = {
+    errors: {
 
-console.log(inputRadioBtn)
-console.log(inputDate)
-console.log(document.querySelector('.js-input-checkbox'))
+    },
+}
 
-// function validateEmail(email) {
-//     let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return re.test(String(email).toLowerCase());
-// }
-//
-// function validateName(name) {
-//     let re = /[a-z]/;
-//     return re.test(String(name).toLowerCase());
-// }
-//
-// function validateCompany(company) {
-//     let re = /[a-z]/;
-//     return re.test(String(company).toLowerCase());
-// }
-//
-// function validateCountry(country) {
-//     let re = /[a-z]/;
-//     return re.test(String(country).toLowerCase());
-// }
-//
-// function validateRadioBtn(select) {
-//     let re = /[a-z]/;
-//     return re.test(String(select).toLowerCase());
-// }
-//
-// function validateDate(date) {
-//     let re = /[a-z]/;
-//     return re.test(String(date).toLowerCase());
-// }
-
-let radioValue
-
-const radioBtn = document.getElementsByName('1');
 for (let i = 0, length = radioBtn.length; i < length; i++) {
     radioBtn[i].onclick = (e) => {
        radioValue = e.target.value
    }
 }
 
-btnNext.onclick = () => {
-    form1.classList.add('hidden')
-    switch (radioValue) {
-        case '1' :  form2.classList.remove('hidden')
-            break;
-        case '2' :   form3.classList.remove('hidden')
-            break;
-        case '3' :   form4.classList.remove('hidden')
-            break;
-    }
-}
-console.log(btnBack)
 for (let i = 0, length = btnBack.length; i < length; i++) {
     btnBack[i].onclick = (e) => {
         switch (e.target.value) {
@@ -81,6 +38,37 @@ for (let i = 0, length = btnBack.length; i < length; i++) {
                 break;
             case '3' :   form1.classList.remove('hidden')
                 form4.classList.add('hidden')
+                break;
+        }
+    }
+}
+
+btnNext.onclick = () => {
+    form1.querySelectorAll("input.js-input").forEach((item) => {
+        if(!validation(item)) {
+            state.errors[item.name] = true;
+        } else {
+            delete state.errors[item.name];
+        }
+    })
+
+    if(!Boolean(radioValue)) {
+        state.errors.serviceType = true;
+        inputRadioBtn.closest('.form__group').classList.add('error')
+    } else {
+        delete state.errors.serviceType;
+        inputRadioBtn.closest('.form__group').classList.remove('error')
+    }
+
+    if(!Object.keys(state.errors).length) {
+        form1.classList.add('hidden')
+
+        switch (radioValue) {
+            case '1' :  form2.classList.remove('hidden')
+                break;
+            case '2' :   form3.classList.remove('hidden')
+                break;
+            case '3' :   form4.classList.remove('hidden')
                 break;
         }
     }
@@ -162,4 +150,23 @@ form.onsubmit = function (e) {
     // } else {
     //     inputDate.classList.remove('error');
     // }
+}
+
+function validation(element){
+    switch (element.name) {
+        case 'email':
+            // let parentBlock = element.closest('.form__group')
+            // state.errors[element.name] = validateEmail(element.value)
+            // state.errors[element.name] ?
+            //     parentBlock.closest('.form__group').classList.add('error')
+            //     : parentBlock.closest('.form__group').classList.remove('error')
+            return validateEmail(element.value)
+        default :
+            return element.value !== ''
+    }
+}
+
+function validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
